a37c1645a2236735b2d186ccdcbb5cc344488302
MKDramdahl <dramd002@morris.umn.edu>
2014-01-20 16:10:47 -0600

    Extracted Machine class from MachineSHopSimulator.  All tests pass.

--- src/applications/MachineShopSimulator.java
+++ src/applications/MachineShopSimulator.java
@@ -17,20 +17,6 @@ public class MachineShopSimulator {
     // top-level nested classes
 
 
-    private static class Machine {
-        // data members
-        LinkedQueue jobQ; // queue of waiting jobs for this machine
-        int changeTime; // machine change-over time
-        int totalWait; // total delay at this machine
-        int numTasks; // number of tasks processed on this machine
-        Job activeJob; // job currently active on this machine
-
-        // constructor
-        private Machine() {
-            jobQ = new LinkedQueue();
-        }
-    }
-
     private static class EventList {
         // data members
         int[] finishTime; // finish time array
